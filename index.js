0131ac22f9195df1b875429600e13ae2d1e867af
stanley <stanley.njuguna@student.moringaschool.com>
2024-01-14 09:35:18 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+const body = document.createTextNode("Hello, World!");
+newHeader.appendChild(body);
+// document.body.append(newHeader)
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = "Alvin is the champion" 
